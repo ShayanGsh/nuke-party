f2b4a538bc962f989f99f37392b609bad70ad82d
ShayanGsh <shayan1997g@gmail.com>
2024-03-06 15:18:46 +0330

    :construction: Added stage and viewport, working on the UI

--- core/src/dev/gash/game/Game.java
+++ core/src/dev/gash/game/Game.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
@@ -18,9 +19,12 @@ import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.TimeUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import dev.gash.game.controller.OrthoCamController;
 import dev.gash.game.map.PerlinMapGenerator;
 import dev.gash.game.map.TileSelector;
@@ -32,17 +36,18 @@ public class Game extends ApplicationAdapter {
 	private TiledMap map;
 	private MapRenderer mapRenderer;
 	private OrthoCamController cameraController;
-
-
+	private Stage stage;
 	@Override
 	public void create() {
 
-		// create the camera and the SpriteBatch
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 1366, 768);
+		camera.setToOrtho(false, 1920, 1080);
 		cameraController = new OrthoCamController(camera);
-		Gdx.input.setInputProcessor(cameraController);
 		batch = new SpriteBatch();
+		stage = new Stage(new ScreenViewport(camera));
+
+		var inputMultiplexer = new InputMultiplexer(stage, cameraController);
+		Gdx.input.setInputProcessor(inputMultiplexer);
 
 		map = new PerlinMapGenerator(200, 200)
 				.generate(new GroundLayer());
