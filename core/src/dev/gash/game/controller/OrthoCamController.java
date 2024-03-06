44865d54adcff6246b6af9dd1646e9c8cf5fef51
ShayanGsh <shayan1997g@gmail.com>
2024-03-06 11:55:04 +0330

    :sparkles: Added faster zooming by holding CTRL

--- core/src/dev/gash/game/controller/OrthoCamController.java
+++ core/src/dev/gash/game/controller/OrthoCamController.java
@@ -1,5 +1,6 @@
 package dev.gash.game.controller;
 
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector3;
@@ -10,12 +11,14 @@ public class OrthoCamController extends InputAdapter {
     final Vector3 last = new Vector3(-1, -1, -1);
     final Vector3 delta = new Vector3();
 
-    public OrthoCamController (OrthographicCamera camera) {
+    float zoomAcceleration = 0.1f;
+
+    public OrthoCamController(OrthographicCamera camera) {
         this.camera = camera;
     }
 
     @Override
-    public boolean touchDragged (int x, int y, int pointer) {
+    public boolean touchDragged(int x, int y, int pointer) {
         camera.unproject(curr.set(x, y, 0));
         if (!(last.x == -1 && last.y == -1 && last.z == -1)) {
             camera.unproject(delta.set(last.x, last.y, 0));
@@ -27,14 +30,31 @@ public class OrthoCamController extends InputAdapter {
     }
 
     @Override
-    public boolean touchUp (int x, int y, int pointer, int button) {
+    public boolean touchUp(int x, int y, int pointer, int button) {
         last.set(-1, -1, -1);
         return false;
     }
 
     @Override
+    public boolean keyDown(int keycode) {
+        if (keycode == Input.Keys.CONTROL_LEFT || keycode == Input.Keys.CONTROL_RIGHT) {
+            zoomAcceleration = 1.0f;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        if (keycode == Input.Keys.CONTROL_LEFT || keycode == Input.Keys.CONTROL_RIGHT) {
+            zoomAcceleration = 0.1f;
+        }
+        return false;
+    }
+
+    @Override
     public boolean scrolled(float amountX, float amountY) {
-        camera.zoom += amountY / 10;
+
+        camera.zoom += amountY * zoomAcceleration;
         return false;
     }
 }
